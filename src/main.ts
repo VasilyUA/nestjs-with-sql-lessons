1be1db1c66541b4a0095d7e03e1eac56fe17c49f
vasilyua <mironenkovasila@gmail.com>
2022-08-16 13:49:26 +0300

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const PORT: number | string = process.env.PORT || 5000;
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*', optionsSuccessStatus: 204 });
   const config = new DocumentBuilder().setTitle('nest-lessons').setDescription('The API description').setVersion('1.0.0').addTag('VSUA').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/docs', app, document);
